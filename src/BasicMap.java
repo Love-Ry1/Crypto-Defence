0d9571c0901fc793c5d87142e8ad1e49a8979ec1
abbas <faizi@student.chalmers.se>
2022-01-31 15:17:33 +0100

    change some logic i basicmap

--- src/BasicMap.java
+++ src/BasicMap.java
@@ -19,13 +19,13 @@ public class BasicMap {
     Block[][] blockArray = new Block[width][height];
 
     public BasicMap(){
-        for(int i = 0; i < this.width; i++){        // loops through the block array
-            for(int j = 0; j < this.height; j++){
+        for(int i = 0; i < this.height; i++){        // loops through the block array
+            for(int j = 0; j < this.width; j++){
                 if (blockTypeArray[i][j] == 0) {
-                    blockArray[i][j] = new GrassBlock(i * Block.getWidth(), j * Block.getHeight());
+                    blockArray[i][j] = new GrassBlock(j * Block.getWidth(), i * Block.getHeight());
                 }
                 else if(blockTypeArray[i][j] == 1){
-                    blockArray[i][j] = new StoneBlock(i * Block.getWidth(), j * Block.getHeight());
+                    blockArray[i][j] = new StoneBlock(j * Block.getWidth(), i * Block.getHeight());
                 }
             }
         }
