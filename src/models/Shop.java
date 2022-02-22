7cd5d6a5e1ce1866c93b4f48f4eea36b46b7439e
abbas <faizi@student.chalmers.se>
2022-02-22 20:05:09 +0100

    added javadoc to some methods

--- src/models/Shop.java
+++ src/models/Shop.java
@@ -12,11 +12,20 @@ public class Shop {
         this.gameModel = gameModel;
     }
 
+    /**
+     * This method is for setting the button on the map
+     * @param button1
+     */
     public void setButton1(boolean button1){
         this.button1 = button1;
         System.out.println("button 1 is true");
     }
 
+    /**
+     * This method is for setting the coordinates
+     * @param x
+     * @param y
+     */
     public void setCoordinates(int x, int y){
         if (button1) {
             this.posX = x;
@@ -24,20 +33,35 @@ public class Shop {
         }
     }
 
+    /**
+     * This method returns position x
+     * @return
+     */
     public int getPosX(){
         return posX;
     }
 
+    /**
+     * This methods returns position y
+     * @return
+     */
     public int getPosY(){
         return posY;
     }
 
+    /**
+     * This method reset the position x,y and button
+     */
     public void reset(){
         posX = -1;
         posY = -1;
         button1 = false;
     }
 
+    /**
+     * This method add a new Tower on the map
+     * @return
+     */
     public boolean addTower(){
         return (button1 && posX >= 0 && posY >= 0);
     }
--- src/models/Tower.java
+++ src/models/Tower.java
@@ -19,68 +19,135 @@ public abstract class Tower {
     }
     private TowerName towerName;
 
+    /**
+     * This method sets the position x and y for the tower
+     *
+     * @param posX
+     * @param posY
+     */
     protected Tower(int posX, int posY){
         this.posX = posX;
         this.posY = posY;
     }
 
+    /**
+     * This method sets the damage
+     * @param damage
+     */
     public void setDamage(int damage){
         this.damage = damage;
     }
 
+    /**
+     * This method return the damage
+     * @return
+     */
     public int getDamage(){
         return this.damage;
     }
 
+    /**
+     * this method sets the range
+     * @param range
+     */
     public void setRange(int range){
         this.range = range;
     }
 
+    /**
+     * This method returns the range
+     * @return
+     */
     public int getRange(){
         return this.range;
     }
 
+    /**
+     * This method sets ................
+     * @param coolDown
+     */
     public void setAttackCoolDown(int coolDown){
         attackCoolDown = coolDown;
     }
 
+    /**
+     * This method set the cost
+     * @param cost
+     */
     public void setCost(int cost) {
         this.cost = cost;
     }
 
+    /**
+     * This method return the cost
+     * @return
+     */
     public int getCost(){
         return this.cost;
     }
 
+    /**
+     * This method return the position x
+     * @return
+     */
     public int getPosX(){
         return this.posX;
     }
 
+    /**
+     * This method return the position Y
+     * @return
+     */
     public int getPosY(){
         return this.posY;
     }
 
+    /**
+     * This method set the image of the tower
+     * @param image
+     */
     public void setTowerImage(Image image){
         this.towerImage = image;
     }
 
+    /**
+     * This method return image of the tower
+     * @return
+     */
     public Image getTowerImage(){
         return this.towerImage;
     }
 
+    /**
+     * This method return name of the tower
+     * @return
+     */
     public TowerName getTowerName() {
         return towerName;
     }
 
+    /**
+     * This method sets a name to the tower
+     * @param towerName
+     */
     public void setTowerName(TowerName towerName) {
         this.towerName = towerName;
     }
 
+    /**
+     * This method shoot the bullet toward the enemy by seting a specific coordinates
+     * @param enemy
+     */
     public void shoot(Enemy enemy){    // Do this abstract or use bullet type so different towers can use different bullets
         // also need a method to calculate the direction
         bullets.add(new BasicBullet(posX + Block.getWidth()/2, posY, Bullets.Direction.NORTH, enemy));
     }
 
+    /**
+     * This method return the first enemy which is in the range,
+     * @param enemyList
+     * @return
+     */
     // returnerar första enemyn som är i range, är inte perfekt implementerad eftersom models.Enemy klassen inte är klar än
     public Enemy firstEnemyInRange(ArrayList<Enemy> enemyList){
         for (int i = 0; i < enemyList.size(); i++){
@@ -95,10 +162,18 @@ public abstract class Tower {
         return null;
     }
 
+    /**
+     * This method return the bullets
+     * @return
+     */
     public ArrayList<Bullets> getBullets(){
         return bullets;
     }
 
+    /**
+     * This method update the state of the bullet and shooting the first enemy
+     * @param enemyList
+     */
     public void update(ArrayList<Enemy> enemyList){
         Enemy firstEnemy = firstEnemyInRange(enemyList);
         if(firstEnemy != null && attackCoolDownTick > (attackCoolDown * 5)){
