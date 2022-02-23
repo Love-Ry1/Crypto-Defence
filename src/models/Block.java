34c96e7202c7d4a885b923d1053808d9145d60d1
abbas <faizi@student.chalmers.se>
2022-02-23 22:50:59 +0100

    added javadoc to some methods

--- src/models/Block.java
+++ src/models/Block.java
@@ -9,31 +9,60 @@ public abstract class Block {
     private static final int width = 80;     // width is 50 for all blocks (temp value)
     private static final int height = 80;    // height is 50 for all blocks (temp value)
 
+    /**
+     * Constructor for initiate and create the block
+     * @param posX
+     * @param posY
+     */
     protected Block(int posX, int posY) {
         this.posX = posX;
         this.posY = posY;
     }
 
+    /**
+     * This method return x position of the block
+     * @return
+     */
     public int getPosX() {
         return posX;
     }
 
+    /**
+     * This method return y position of the block
+     * @return
+     */
     public int getPosY() {
         return posY;
     }
 
+    /**
+     * This method return width of the block
+     * @return
+     */
     public static int getWidth() {
         return width;
     }
 
+    /**
+     * This method returns height of the block
+     * @return
+     */
     public static int getHeight() {
         return height;
     }
 
+    /**
+     * This method is for att assign image to the block
+     * @param image
+     */
     public void setBlockImage(Image image) {
         this.blockImage = image;
     }
 
+    /**
+     * This method returns image of the block.
+     * @return
+     */
     public Image getBlockImage() {
         return this.blockImage;
     }
