85e2e8773721b5c9735638e78266274e39fbef53
Mustafa0134 <saffobawi@hotmail.se>
2022-03-06 02:46:11 -1200

    more tests are written

--- test/models/ModelTest.java
+++ test/models/ModelTest.java
@@ -1,18 +1,44 @@
 package models;
-/*
+
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class ModelTest {
     @Test
     public void towersShootsBulletsAtEnemies() {
         Tower tower = new BasicTower(0, 0);
-        Enemy enemy = new Enemy1(80, 0);
+        BasicMap basicMap = new BasicMap();
+        MobWave mobWave = new MobWave(basicMap);
+        Enemy enemy = new Enemy1(80, 0, mobWave);
 
         assertEquals(0, tower.getBullets().size());
         tower.shoot(enemy);
         assertTrue(tower.getBullets().size() > 0);
     }
-}*/
+    @Test
+    public void playerTakeDamage(){
+        Player player = new Player();
+        int OldHealth = player.getHealth();
+        BasicMap basicMap = new BasicMap();
+        MobWave mobWave = new MobWave(basicMap);
+
+        assertEquals(100, player.getHealth());
+
+        player.takeDamage(10);
+        assertEquals(player.getHealth(), OldHealth - 10);
+   }
+
+   @Test
+   public void addTowerOnMapWithShop(){
+        Shop shop = new Shop();
+        assertFalse(shop.addTower());
+        shop.setButton1(true);
+        shop.setCoordinates(100,100);
+        assertTrue(shop.addTower());
+   }
+
+
+
+
+}
